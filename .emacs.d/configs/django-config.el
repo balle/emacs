9a3708350031fa73303e7f41005a4c62a671efce
Bastian Ballmann <bastian.ballmann@notch-interactive.com>
2011-09-12 09:25:46 +0200

    django-mode

--- .emacs.d/configs/django-config.el
+++ .emacs.d/configs/django-config.el
@@ -1,28 +1,32 @@
 (require 'pony-mode)
-(add-hook 'html-mode-hook '(lambda () (define-key ac-complete-mode-map "\t" 'ac-expand)))
-;(add-hook 'pony-minor-mode-hook '(lambda ()
+(add-hook 'pony-minor-mode-hook '(lambda ()
 ;	(define-key pony-minor-mode-map [f2] 'pony-syncdb)
 ;	(define-key pony-minor-mode-map [f3] 'balle-django-migrate)
-;))
+))
+
 (require 'flymake)
 (defun flymake-html-init ()
-          (let* ((temp-file (flymake-init-create-temp-buffer-copy
-                             'flymake-create-temp-inplace))
-                 (local-file (file-relative-name
-                              temp-file
-                              (file-name-directory buffer-file-name))))
-            (list "balle_tidy" (list local-file))))
+	  (let* ((temp-file (flymake-init-create-temp-buffer-copy
+	                     'flymake-create-temp-inplace))
+	         (local-file (file-relative-name
+	                      temp-file
+	                      (file-name-directory buffer-file-name))))
+	    (list "balle_tidy" (list local-file))))
 
-(add-to-list 'flymake-allowed-file-name-masks
-             '("\\.html$\\|\\.htm" flymake-html-init))
+;(add-to-list 'flymake-allowed-file-name-masks
+;	     '("\\.html$\\|\\.htm" flymake-html-init))
 
 (add-to-list 'flymake-err-line-patterns
-             '("line \\([0-9]+\\) column \\([0-9]+\\) - \\(Warning\\|Error\\): \\(.*\\)"
-               nil 1 2 4))
+	     '("line \\([0-9]+\\) column \\([0-9]+\\) - \\(Warning\\|Error\\): \\(.*\\)"
+	       nil 1 2 4))
 
-;(add-to-list 'load-path "~/.emacs.d/extensions/django-mode/")
-;(require 'django-html-mode)
-;(require 'django-mode)
-;(yas/load-directory "~/.emacs.d/extensions/django-mode/snippets")
-;(add-to-list 'auto-mode-alist '("\\.html$" . django-html-mode))
-(add-to-list 'ac-modes 'html-mode)
+(require 'django-html-mode)
+(require 'django-mode)
+(add-to-list 'auto-mode-alist '("\\.html$" . django-html-mode))
+(add-to-list 'ac-modes 'django-html-mode)
+
+(add-hook 'django-html-mode-hook '(lambda ()
+    (define-key django-html-mode-map "\C-c\C-d" 'django-html-close-tag)
+))
+
+
