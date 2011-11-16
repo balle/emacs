24279d891b3a016f29341c460eda945ad74f7c2a
Bastian Ballmann <bastian.ballmann@notch-interactive.com>
2011-11-16 16:45:38 +0100

    better django mode

--- .emacs.d/configs/django-config.el
+++ .emacs.d/configs/django-config.el
@@ -20,14 +20,29 @@
 	     '("line \\([0-9]+\\) column \\([0-9]+\\) - \\(Warning\\|Error\\): \\(.*\\)"
 	       nil 1 2 4))
 
-;(require 'django-html-mode)
-(require 'django-mode)
-(add-to-list 'ac-modes 'django-mode)
-;(add-to-list 'auto-mode-alist '("\\.html$" . django-html-mode))
-;(add-to-list 'ac-modes 'django-html-mode)
+(require 'django-html-mode)
+;(require 'django-mode)
+;(add-to-list 'ac-modes 'django-mode)
+(add-to-list 'auto-mode-alist '("\\.html$" . django-html-mode))
+(add-to-list 'ac-modes 'django-html-mode)
 
-;(add-hook 'django-html-mode-hook '(lambda ()
-;    (define-key django-html-mode-map "\C-c\C-d" 'django-html-close-tag)
-;))
+(load "~/.emacs.d/extensions/nxhtml/autostart.el")
+(setq mumamo-background-colors nil)
+(add-to-list 'auto-mode-alist '("\\.html$" . django-html-mumamo-mode))
+
+;; Mumamo is making emacs 23.3 freak out:
+(when (and (equal emacs-major-version 23)
+           (equal emacs-minor-version 3))
+  (eval-after-load "bytecomp"
+    '(add-to-list 'byte-compile-not-obsolete-vars
+                  'font-lock-beginning-of-syntax-function))
+  ;; tramp-compat.el clobbers this variable!
+  (eval-after-load "tramp-compat"
+    '(add-to-list 'byte-compile-not-obsolete-vars
+                  'font-lock-beginning-of-syntax-function)))
+
+(add-hook 'django-html-mode-hook '(lambda ()
+    (define-key django-html-mode-map "\C-c\C-d" 'django-html-close-tag)
+))
 
 
