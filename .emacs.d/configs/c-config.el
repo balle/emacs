f16de881bf2657e6eef5fa11c0d6cea674774245
Bastian Ballmann <bastiaba@pulsar.inf.ethz.ch>
2012-09-26 11:47:35 +0200

    c config

---
+++ .emacs.d/configs/c-config.el
@@ -0,0 +1,41 @@
+(add-to-list 'load-path "~/.emacs.d/extensions/cedet-1.1")
+(load-file "~/.emacs.d/extensions/cedet-1.1/common/cedet.el")
+
+;; needed for completion
+(require 'semantic-ia)
+
+;; needed for glibc includes
+(require 'semantic-gcc)
+
+;; * This enables the database and idle reparse engines
+(semantic-load-enable-minimum-features)
+
+;; * This enables some tools useful for coding, such as summary mode,
+;;   imenu support, and the semantic navigator
+(semantic-load-enable-code-helpers)
+
+;; enables semantic-stickyfunc-name that displays name of current function
+;; in topmost line of buffer, semantic-decoration-mode to decorate tags,
+;; using different styles, and semantic-idle-completion-mode for automatic
+;; generation of possible names completions, if user stops his work for some time
+(semantic-load-enable-gaudy-code-helpers)
+
+; Enable template insertion menu
+(global-srecode-minor-mode 1)
+
+;; CC-mode
+(add-hook 'c-mode-hook '(lambda ()
+    (setq ac-sources (append '(ac-source-semantic) ac-sources))
+    (local-set-key (kbd "RET") 'newline-and-indent)
+    (local-set-key (kbd "TAB") 'semantic-ia-complete-symbol-menu)
+    (local-set-key "C-c d" 'semantic-ia-show-doc)
+    (local-set-key "C-c g" 'semantic-ia-fast-jump)
+    (local-set-key "C-d" 'kill-whole-line)
+    (linum-mode t)
+    (flymake-mode)
+))
+
+;; Autocomplete
+(add-to-list 'ac-modes 'c-mode)
+
+(add-to-list 'semantic-lex-c-preprocessor-symbol-file "/usr/include/stdlib.h")
