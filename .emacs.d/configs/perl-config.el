49df2f9836123fd593d8291afc568c803b4ff8b1
Bastian Ballmann <bastiaba@pulsar.inf.ethz.ch>
2012-11-27 13:47:43 +0100

    better perl mode

--- .emacs.d/configs/perl-config.el
+++ .emacs.d/configs/perl-config.el
@@ -1,5 +1,22 @@
 (add-to-list 'load-path "~/.emacs.d/extensions/Emacs-PDE-0.2.16/lisp")
 
+;; Use cperl-mode instead of the default perl-mode
+(add-to-list 'auto-mode-alist '("\\.\\([pP][Llm]\\|al\\)\\'" . cperl-mode))
+(add-to-list 'interpreter-mode-alist '("perl" . cperl-mode))
+(add-to-list 'interpreter-mode-alist '("perl5" . cperl-mode))
+(add-to-list 'interpreter-mode-alist '("miniperl" . cperl-mode))
+
+(add-hook 'cperl-mode-hook 'n-cperl-mode-hook t)
+(add-hook 'cperl-mode-hook 'flymake-mode)
+
+(defun n-cperl-mode-hook ()
+    (setq cperl-indent-level 4)
+    (setq cperl-continued-statement-offset 0)
+    (setq cperl-extra-newline-before-brace t)
+    (set-face-background 'cperl-array-face "wheat")
+    (set-face-background 'cperl-hash-face "wheat")
+)
+
 (require 'perl-completion)
 (load "pde-load")
 
@@ -10,5 +27,5 @@
   )
 )
 
-;(add-hook 'cperl-mode-hook 'flymake-mode)
+
 
