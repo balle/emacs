8212696bd33425027052725b72f8cc88073da76b
Bastian Ballmann <bastian.ballmann@inf.ethz.ch>
2013-02-07 15:42:12 +0100

    better perl mode

--- .emacs.d/configs/perl-config.el
+++ .emacs.d/configs/perl-config.el
@@ -1,4 +1,5 @@
 (add-to-list 'load-path "~/.emacs.d/extensions/Emacs-PDE-0.2.16/lisp")
+(add-to-list 'load-path "~/.emacs.d/extensions/emacs-flymake-perlcritic")
 ;(add-to-list 'load-path "~/.emacs.d/extensions/stylish-elisp")
 
 ;; Use cperl-mode instead of the default perl-mode
@@ -12,10 +13,17 @@
 (require 'perlcritic)
 (load "pde-load")
 
-(add-hook 'cperl-mode-hook 'n-cperl-mode-hook t)
 (add-hook 'cperl-mode-hook 'flymake-mode)
 
-(defun n-cperl-mode-hook ()
+(setq flymake-perlcritic-command
+        "~/.emacs.d/extensions/emacs-flymake-perlcritic/bin/flymake_perlcritic")
+(setq flymake-perlcritic-severity 1)
+(require 'flymake-perlcritic)
+
+;(add-to-list 'flymake-allowed-file-name-masks
+;             (list "\\.pl\\'" (apply-partially 'flymake-command-parse "perlcritic -1 %f")))
+
+(defun cperl-mode-hook ()
     (setq cperl-indent-level 4)
     (setq cperl-continued-statement-offset 0)
     (setq cperl-extra-newline-before-brace t)
@@ -25,7 +33,7 @@
     (set-face-background 'cperl-hash-face "wheat")
 
     (perl-completion-mode t)
-    (flymake-mode)
+    (flymake-mode t)
 
     (linum-mode t)
     (setq skeleton-pair t)
