018e32ff5acfcae4298ebb7c2eebdfdf7d374fbd
Bastian Ballmann <bastian.ballmann@inf.ethz.ch>
2013-02-08 13:20:36 +0100

    better perl completion

--- .emacs.d/configs/perl-config.el
+++ .emacs.d/configs/perl-config.el
@@ -1,6 +1,5 @@
 (add-to-list 'load-path "~/.emacs.d/extensions/Emacs-PDE-0.2.16/lisp")
 (add-to-list 'load-path "~/.emacs.d/extensions/emacs-flymake-perlcritic")
-;(add-to-list 'load-path "~/.emacs.d/extensions/stylish-elisp")
 
 ;; Use cperl-mode instead of the default perl-mode
 (add-to-list 'auto-mode-alist '("\\.\\([pP][Llm]\\|al\\)\\'" . cperl-mode))
@@ -8,11 +7,11 @@
 (add-to-list 'interpreter-mode-alist '("perl5" . cperl-mode))
 (add-to-list 'interpreter-mode-alist '("miniperl" . cperl-mode))
 
-;(require 'stylish)
 (require 'perl-completion)
 (require 'perlcritic)
 (load "pde-load")
 
+(add-hook 'cperl-mode-hook 'n-cperl-mode-hook)
 (add-hook 'cperl-mode-hook 'flymake-mode)
 
 (setq flymake-perlcritic-command
@@ -23,7 +22,15 @@
 ;(add-to-list 'flymake-allowed-file-name-masks
 ;             (list "\\.pl\\'" (apply-partially 'flymake-command-parse "perlcritic -1 %f")))
 
-(defun cperl-mode-hook ()
+(add-hook  'n-cperl-mode-hook
+           (lambda ()
+             (when (require 'auto-complete nil t) ; no error whatever auto-complete.el is not installed.
+               (auto-complete-mode t)
+               (make-variable-buffer-local 'ac-sources)
+               (setq ac-sources
+                     '(ac-source-perl-completion)))))
+
+(defun n-cperl-mode-hook ()
     (setq cperl-indent-level 4)
     (setq cperl-continued-statement-offset 0)
     (setq cperl-extra-newline-before-brace t)
@@ -34,9 +41,13 @@
 
     (perl-completion-mode t)
     (flymake-mode t)
-
+    (outline-minor-mode)
     (linum-mode t)
     (setq skeleton-pair t)
+
+    (global-set-key [f10] 'flymake-goto-prev-error)
+    (global-set-key [f11] 'flymake-goto-next-error)
+
     (global-set-key "(" 'skeleton-pair-insert-maybe)
     (global-set-key "[" 'skeleton-pair-insert-maybe)
     (global-set-key "{" 'skeleton-pair-insert-maybe)
