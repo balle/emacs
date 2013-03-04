2d59379e5b8ebac9bda7978289b7c51c6b5e8c38
Bastian Ballmann <bastian.ballmann@inf.ethz.ch>
2013-03-04 16:52:16 +0100

    spell checking with hunspell

--- .emacs.d/configs/spell-checking-config.el
+++ .emacs.d/configs/spell-checking-config.el
@@ -2,37 +2,33 @@
 (require 'ispell)
 
 (setq ispell-dictionary-base-alist
-  '(
- 	("de_DE"
- 	 "[a-zäöüßA-ZÄÖÜ]" "[^a-zäöüßA-ZÄÖÜ]" "[']" nil
- 	 ("-d" "de_DE") nil utf-8)
-	
- 	("de_CH"
- 	 "[a-zäöüA-ZÄÖÜ]" "[^a-zäöüA-ZÄÖÜ]" "[']" nil
- 	 ("-d" "de_CH") nil utf-8)
-	
- 	("en_US"
- 	 "[a-zA-Z]" "[^a-zA-Z]" "[']" nil
- 	 ("-d" "en_US") nil utf-8)
-	
- 	("en_GB"
- 	 "[a-zA-Z]" "[^a-zA-Z]" "[']" nil
- 	 ("-d" "en_GB") nil utf-8)
-	
-    )
-)
+        '(
+                  ("de_DE"
+                            "[a-zäöüßA-ZÄÖÜ]" "[^a-zäöüßA-ZÄÖÜ]" "[']" nil
+                            ("-d" "de_DE" "-i" "iso-8859-1") nil iso-8859-1)
+
+                  ("en_US"
+                            "[a-zA-Z]" "[^a-zA-Z]" "[']" nil
+                            ("-d" "en_US" "-i" "iso-8859-1") nil iso-8859-1)
+
+                  ("en_GB"
+                            "[a-zA-Z]" "[^a-zA-Z]" "[']" nil
+                            ("-d" "en_GB" "-i" "iso-8859-1") nil iso-8859-1)
+
+              )
+        )
 
 (eval-after-load "ispell"
-    (progn
-         (setq ispell-dictionary "de_DE")
-	 (setq ispell-extra-args '("-i" "utf-8"))
-	 (setq ispell-silently-savep t)
-     )
-)
+      (progn
+        (setq ispell-dictionary "de_DE")
+        ;(setq ispell-extra-args '("-t")) ; input is tex or latex
+        (setq ispell-silently-savep t)   ; save personal dict without confirmation
+                               )
+      )
 
-;(setq flyspell-default-dictionary "swissgerman")
 (setq-default ispell-program-name "hunspell")
 (setq ispell-really-hunspell t)
+(setq ispell-skip-html t)
 
 (defun turn-on-flyspell-mode ()
    (flyspell-mode 1)
