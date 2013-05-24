a90c9aa2c5bfdc222f83639b54ab9f08575fd8c1
Bastian Ballmann <bastian.ballmann@inf.ethz.ch>
2013-05-24 13:00:43 +0200

    ipython config

--- .emacs.d/configs/python-config.el
+++ .emacs.d/configs/python-config.el
@@ -32,3 +32,6 @@
         (setq ansi-color-for-comint-mode t)
 ))
 
+(setq python-shell-completion-setup-code "from IPython.core.completerlib import module_completion"
+      python-shell-completion-module-string-code "';'.join(module_completion('''%s'''))\n"
+      python-shell-completion-string-code "';'.join(get_ipython().Completer.all_completions('''%s'''))\n")
