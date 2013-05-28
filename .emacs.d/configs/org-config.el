ef239ba3b5ecc96dd0e095694108d9e57fca0ac1
Bastian Ballmann <balle@chaostal.de>
2013-05-28 21:08:01 +0200

    show emacs.org with C-xte

--- .emacs.d/configs/org-config.el
+++ .emacs.d/configs/org-config.el
@@ -51,7 +51,7 @@
 ; functions to show todos / notes / appointments
 (defun balle-show-emacs-list ()
   (interactive)
-  (balle-org-tags-view "emacs")
+  (org-tags-view nil "emacs")
   (next-multiframe-window)
   (enlarge-window 10)
 )
@@ -65,28 +65,28 @@
 
 (defun balle-show-notes-list ()
   (interactive)
-  (balle-org-tags-view "note")
+  (org-tags-view nil "note")
   (next-multiframe-window)
   (enlarge-window 10)
 )
 
 (defun balle-show-read-list ()
   (interactive)
-  (balle-org-tags-view "read")
+  (org-tags-view nil "read")
   (next-multiframe-window)
   (enlarge-window 10)
 )
 
 (defun balle-show-hack-list ()
   (interactive)
-  (balle-org-tags-view "hacking")
+  (org-tags-view nil "hacking")
   (next-multiframe-window)
   (enlarge-window 10)
 )
 
 (defun balle-show-python-list ()
   (interactive)
-  (balle-org-tags-view "python")
+  (org-tags-view nil "python")
   (next-multiframe-window)
   (enlarge-window 10)
 )
@@ -110,72 +110,6 @@
   (org-todo-list "STARTED")
   (next-multiframe-window)
   (enlarge-window 10)
-)
-
-; modified version of org-tags-view to display only
-(defun balle-org-tags-view (match)
-  (interactive "p")
-  (org-compile-prefix-format 'tags)
-  (org-set-sorting-strategy 'tags)
-  (let* ((org-tags-match-list-sublevels
-	  org-tags-match-list-sublevels)
-	 (completion-ignore-case t)
-	 rtn rtnall files file pos matcher
-	 buffer)
-    (setq matcher (org-make-tags-matcher match)
-	  match (car matcher) matcher (cdr matcher))
-    (org-prepare-agenda (concat "TAGS " match))
-    (setq org-agenda-query-string match)
-    (setq org-agenda-redo-command
-	  (list 'balle-org-tags-view ()
-		(list 'if 'current-prefix-arg nil 'org-agenda-query-string)))
-    (setq files (org-agenda-files nil 'ifmode)
-	  rtnall nil)
-    (while (setq file (pop files))
-      (catch 'nextfile
-	(org-check-agenda-file file)
-	(setq buffer (if (file-exists-p file)
-			 (org-get-agenda-file-buffer file)
-		       (error "No such file %s" file)))
-	(if (not buffer)
-	    ;; If file does not exist, error message to agenda
-	    (setq rtn (list
-		       (format "ORG-AGENDA-ERROR: No such org-file %s" file))
-		  rtnall (append rtnall rtn))
-	  (with-current-buffer buffer
-	    (unless (org-mode-p)
-	      (error "Agenda file %s is not in `org-mode'" file))
-	    (save-excursion
-	      (save-restriction
-		(if org-agenda-restrict
-		    (narrow-to-region org-agenda-restrict-begin
-				      org-agenda-restrict-end)
-		  (widen))
-		(setq rtn (org-scan-tags 'agenda matcher nil))
-		(setq rtnall (append rtnall rtn))))))))
-    (if org-agenda-overriding-header
-	(insert (org-add-props (copy-sequence org-agenda-overriding-header)
-		    nil 'face 'org-agenda-structure) "\n")
-      (insert "Headlines with TAGS match: ")
-      (add-text-properties (point-min) (1- (point))
-			   (list 'face 'org-agenda-structure
-				 'short-heading
-				 (concat "Match: " match)))
-      (setq pos (point))
-      (insert match "\n")
-      (add-text-properties pos (1- (point)) (list 'face 'org-warning))
-      (setq pos (point))
-      (unless org-agenda-multi
-	(insert "Press `C-u r' to search again with new search string\n"))
-      (add-text-properties pos (1- (point)) (list 'face 'org-agenda-structure)))
-    (org-agenda-mark-header-line (point-min))
-    (when rtnall
-      (insert (org-finalize-agenda-entries rtnall) "\n"))
-    (goto-char (point-min))
-    (or org-agenda-multi (org-fit-agenda-window))
-    (add-text-properties (point-min) (point-max) '(org-agenda-type tags))
-    (org-finalize-agenda)
-    (setq buffer-read-only t))
 )
 
 ; send notifications
