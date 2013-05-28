6b5816661157a9f622a546c322103b8408874993
Bastian Ballmann <basti@pulsar-mobile.inf.ethz.ch>
2013-05-28 13:07:23 +0200

    additional package repositories

--- .emacs.d/configs/general-options.el
+++ .emacs.d/configs/general-options.el
@@ -205,8 +205,9 @@
     (progn
       (require 'package)
       (package-initialize)
+      (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/") t)
+      (add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/") t)
       (add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
-      (add-to-list 'package-archives '("marmelade" . "http://marmelade-repo.org/packages/") t)
 ))
 
 (setq confirm-kill-emacs nil)
