d4472f56b00b8a47278f5532585bc44fc231c664
Bastian Ballmann <bastian.ballmann@inf.ethz.ch>
2013-06-05 14:51:39 +0200

    dired config + notes

--- .emacs.d/configs/general-options.el
+++ .emacs.d/configs/general-options.el
@@ -78,6 +78,16 @@
 
 ; dired settings
 ;(setq dired-listing-switches "")
+(setq dired-recursive-copies (quote always))
+(setq dired-recursive-deletes (quote top))
+(add-hook 'dired-mode-hook
+          (lambda ()
+               (define-key dired-mode-map (kbd "<return>")
+                     'dired-find-alternate-file) ; was dired-advertised-find-file
+                 (define-key dired-mode-map (kbd "^")
+                       (lambda () (interactive) (find-alternate-file "..")))
+                   ; was dired-up-directory
+                  ))
 
 ; backup
 (setq
