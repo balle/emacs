2f1fe17f1f561092023cffc9956db3a45c5820d5
Bastian Ballmann <bastian.ballmann@inf.ethz.ch>
2013-06-07 13:18:38 +0200

    better puppet integration

--- .emacs.d/configs/puppet-config.el
+++ .emacs.d/configs/puppet-config.el
@@ -1,3 +1,9 @@
 (require 'puppet-mode)
 (require 'flymake-puppet)
-(add-hook 'puppet-mode-hook (lambda () (flymake-puppet-load)))
+
+(add-to-list 'ac-modes 'puppet-mode)
+(append ac-sources '(ac-source-semantic ac-source-yasnippet))
+
+(add-hook 'puppet-mode-hook
+          (lambda ()
+            (flymake-puppet-load)))
