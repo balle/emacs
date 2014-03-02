a5fd2326e737ba931cffa6a2a3c442043177f3e9
Bastian Ballmann <balle@chaostal.de>
2014-03-02 17:24:52 +0100

    smart umlauts for latex

--- .emacs.d/configs/latex-config.el
+++ .emacs.d/configs/latex-config.el
@@ -28,13 +28,12 @@
 (add-to-list 'ac-modes 'LaTeX-mode)
 (add-to-list 'ac-modes 'latex-mode)
 
-(defun ac-latex-mode-setup ()         ; add ac-sources to default ac-sources
-  (setq ac-sources
-     ;(append '(ac-source-semantic ac-source-math-latex ac-source-latex-commands  ac-source-math-unicode ac-source-yasnippet)
-     (append '(ac-source-latex-commands  ac-source-yasnippet)
-               ac-sources))
-)
-
+(defun ac-LaTeX-mode-setup () ; add ac-sources to default ac-sources
+   (setq ac-sources
+         (append '(ac-source-math-unicode ac-source-math-latex ac-source-latex-commands ac-source-yasnippet)
+                 ac-sources))
+   )
+(add-hook 'LaTeX-mode-hook 'ac-LaTeX-mode-setup)
 (ac-config-default)
 
 
@@ -43,9 +42,11 @@
 (setq reftex-save-parse-info t)
 (setq reftex-use-multiple-selection-buffers t)
 
+(require 'tex-smart-umlauts)
+(add-hook 'LaTeX-mode-hook #'tex-smart-umlauts-decode)
+
 (setq latex-mode-hook
  '(lambda ()
-    (ac-latex-mode-setup)
     (reftex-mode)
     (flyspell-mode)
     (ac-flyspell-workaround)
@@ -54,4 +55,3 @@
     (define-key LaTeX-mode-map "\C-cd" 'LaTeX-find-matching-end)
     (define-key LaTeX-mode-map "\C-ci" 'LaTeX-insert-item)
 ))
-
