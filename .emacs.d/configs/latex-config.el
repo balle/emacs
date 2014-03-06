e6bfdffee2de6b9b2ea0302fcb540cff95942ed9
Bastian Ballmann <bastian.ballmann@inf.ethz.ch>
2014-03-06 12:59:25 +0100

    latex code folding

--- .emacs.d/configs/latex-config.el
+++ .emacs.d/configs/latex-config.el
@@ -25,20 +25,20 @@
 
 ; fold some latex code for better reading
 ; cool stuff from http://www.flannaghan.com/2013/01/11/tex-fold-mode
-(add-hook 'LaTeX-mode-hook 
-      (lambda () 
-        (TeX-fold-mode 1)
-        (add-hook 'find-file-hook 'TeX-fold-buffer t t)
-        (add-hook 'after-change-functions 
-              (lambda (start end oldlen) 
-                (when (= (- end start) 1)
-                  (let ((char-point 
-                                 (buffer-substring-no-properties 
-                                  start end)))
-                   (when (or (string= char-point "}")
-                         (string= char-point "$"))
-                    (TeX-fold-paragraph)))))
-               t t)))
+;; (add-hook 'LaTeX-mode-hook 
+;;       (lambda () 
+;;         (TeX-fold-mode 1)
+;;         (add-hook 'find-file-hook 'TeX-fold-buffer t t)
+;;         (add-hook 'after-change-functions 
+;;               (lambda (start end oldlen) 
+;;                 (when (= (- end start) 1)
+;;                   (let ((char-point 
+;;                                  (buffer-substring-no-properties 
+;;                                   start end)))
+;;                    (when (or (string= char-point "}")
+;;                          (string= char-point "$"))
+;;                     (TeX-fold-paragraph)))))
+;;                t t)))
 
 ; automatically fill and indent
 (add-hook 'LaTeX-mode-hook 'LaTeX-fill-environment)
