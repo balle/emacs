84023455385ab4e54730bdd1a8108336d5060cf2
Bastian Ballmann <bastian.ballmann@inf.ethz.ch>
2014-04-11 16:18:38 +0200

    minor dired enhancements

--- .emacs.d/configs/general-options.el
+++ .emacs.d/configs/general-options.el
@@ -87,9 +87,11 @@
 (setq warning-minimum-level :error)
 
 ; dired settings
+(require 'dired )
 ;(setq dired-listing-switches "")
 (setq dired-recursive-copies (quote always))
 (setq dired-recursive-deletes (quote top))
+(setq dired-dwim-target t)
 (add-hook 'dired-mode-hook
           (lambda ()
                (define-key dired-mode-map (kbd "<return>")
@@ -98,6 +100,8 @@
                        (lambda () (interactive) (find-alternate-file "..")))
                    ; was dired-up-directory
                   ))
+(define-key dired-mode-map (kbd "<return>") 'dired-find-alternate-file) ; was dired-advertised-find-file
+(define-key dired-mode-map (kbd "^") (lambda () (interactive) (find-alternate-file "..")))  ; was dired-up-directory
 
 ; backup
 (setq
