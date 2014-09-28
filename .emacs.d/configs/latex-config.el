5f800c75d66680c8388e75b218821f0c028263bd
Bastian Ballmann <balle@chaostal.de>
2014-09-28 21:46:40 +0200

    debugged latex mode

--- .emacs.d/configs/latex-config.el
+++ .emacs.d/configs/latex-config.el
@@ -2,7 +2,7 @@
 (add-to-list 'load-path "~/.emacs.d/extensions/auctex")
 (add-to-list 'load-path "~/.emacs.d/extensions/ac-math")
 
-(load "~/.emacs.d/extensions/auctex-11.87/tex-site.el" nil t t)
+(load "~/.emacs.d/extensions/auctex/tex-site.el" nil t t)
 (require 'reftex)
 (require 'preview-latex)
 
@@ -41,7 +41,7 @@
 ;;                t t)))
 
 ; automatically fill and indent
-(add-hook 'LaTeX-mode-hook 'LaTeX-fill-environment)
+;(add-hook 'LaTeX-mode-hook 'LaTeX-fill-environment)
 
 ; auto-completion
 (require 'ac-math)
@@ -56,7 +56,6 @@
 (add-hook 'LaTeX-mode-hook 'ac-LaTeX-mode-setup)
 (ac-config-default)
 
-
 ;; Make RefTeX faster
 (setq reftex-enable-partial-scans t)
 (setq reftex-save-parse-info t)
@@ -70,6 +69,7 @@
     (reftex-mode)
     (flyspell-mode)
     (ac-flyspell-workaround)
+    (auto-complete-mode)
     (setq ac-delay 0.2)
     (define-key LaTeX-mode-map "\C-cu" 'LaTeX-find-matching-begin)
     (define-key LaTeX-mode-map "\C-cd" 'LaTeX-find-matching-end)
