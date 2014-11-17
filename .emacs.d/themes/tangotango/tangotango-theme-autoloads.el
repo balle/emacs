3840a26863e3a21ea48617993e58dc5de57a956f
Bastian Ballmann <bastian.ballmann@inf.ethz.ch>
2014-11-17 11:16:24 +0100

    tango tango theme

---
+++ .emacs.d/themes/tangotango/tangotango-theme-autoloads.el
@@ -0,0 +1,26 @@
+;;; tangotango-theme-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil "tangotango-theme" "tangotango-theme.el" (21603
+;;;;;;  20102 0 0))
+;;; Generated autoloads from tangotango-theme.el
+
+(and load-file-name (boundp 'custom-theme-load-path) (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
+
+;;;***
+
+;;;### (autoloads nil nil ("tangotango-theme-pkg.el") (21603 20102
+;;;;;;  791152 352000))
+
+;;;***
+
+(provide 'tangotango-theme-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; tangotango-theme-autoloads.el ends here
---
+++ .emacs.d/themes/tangotango/tangotango-theme-pkg.el
@@ -0,0 +1,1 @@
+(define-package "tangotango-theme" "20140727.838" "Tango Palette color theme for Emacs 24." (quote nil))
