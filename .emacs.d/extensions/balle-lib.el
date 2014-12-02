211770035e53d80534a01c81a4a2ad674152de20
Bastian Ballmann <bastian.ballmann@inf.ethz.ch>
2014-12-02 15:40:25 +0100

    always update package cache

--- .emacs.d/extensions/balle-lib.el
+++ .emacs.d/extensions/balle-lib.el
@@ -1,5 +1,6 @@
 (defun install-missing-packages (package-list)
-  (unless package-archive-contents (package-refresh-contents))
+  ;(unless package-archive-contents (package-refresh-contents))
+  (package-refresh-contents)
   (dolist (package package-list)
     (unless (package-installed-p package)
       (package-install package))))
