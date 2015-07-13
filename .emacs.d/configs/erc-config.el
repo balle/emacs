0d1a14dd885c76b071af8c7e1ef1d50ebdb659c0
bastiaba <bastian.ballmann@inf.ethz.ch>
2015-07-13 15:30:42 +0200

    erc config

--- .emacs.d/configs/erc-config.el
+++ .emacs.d/configs/erc-config.el
@@ -20,3 +20,23 @@
 
 (erc-timestamp-mode t)
 (setq erc-timestamp-format "[%R-%m/%d]")
+
+;; (defun start-irc ()
+;;    "Connect to IRC."
+;;    (interactive)
+;;    (erc-tls :server "irc.oftc.net" :port 6697
+;;         :nick "ootput" :full-name "ootput")
+;;    (erc :server "irc.freenode.net" :port 6667
+;;         :nick "ootput" :full-name "ootput")
+;;    (setq erc-autojoin-channels-alist '(("freenode.net" "#emacs" "#screen" "#ion")
+;;                                        ("oftc.net" "#debian"))))
+
+(defun stop-irc ()
+ (interactive)
+ (dolist (buf (buffer-list))
+  (with-current-buffer buf
+    (message "MUH %s" major-mode)
+    (if (eq (prin1-to-string major-mode) "erc-mode")
+      (progn
+	(message "MAEH")
+      (kill-buffer buf))))))
