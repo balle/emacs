1c5150760ac5d6c23579606324243b04c75d7c2d
bastiaba <bastian.ballmann@inf.ethz.ch>
2016-01-14 13:47:44 +0100

    Merge branch 'master' of github.com:balle/emacs

--- .emacs.d/configs/clojure-config.el
+++ .emacs.d/configs/clojure-config.el
@@ -2,3 +2,20 @@
 			    ac-cider
 			    clojure-cheatsheet))
 
+;; cool idea found on http://root42.blogspot.ch/2014/08/how-to-automatically-refresh-cider-when.html
+(add-hook 'cider-mode-hook
+	  '(lambda () (add-hook 'after-save-hook
+				'(lambda ()
+				   (if (and (boundp 'cider-mode) cider-mode)
+				       (cider-namespace-refresh)
+				     )))))
+
+(defun cider-namespace-refresh ()
+  (interactive)
+  (cider-interactive-eval
+        "(require 'clojure.tools.namespace.repl)
+    (clojure.tools.namespace.repl/refresh)"))
+
+;(define-key clojure-mode-map (kbd "C-c C-r") 'cider-namespace-refresh)
+
+
