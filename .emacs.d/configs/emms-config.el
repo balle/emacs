5a530551b85f522e38623229b78736e55e6d1a3c
bastiaba <bastian.ballmann@inf.ethz.ch>
2016-02-19 14:10:48 +0100

    emms streams and modeline

--- .emacs.d/configs/emms-config.el
+++ .emacs.d/configs/emms-config.el
@@ -1,6 +1,16 @@
 (install-missing-packages '(emms))
 
 (require 'emms-setup)
+;(require 'emms-browser)
+(require 'emms-streams)
+(require 'emms-mode-line)
+;(require 'emms-lyrics)
+
 (emms-standard)
 (emms-default-players)
 (setq emms-source-file-default-directory "~/Music/")
+
+
+(emms-mode-line 1)
+;(emms-lyrics 1)
+(emms-browser-make-filter "all" 'ignore)
