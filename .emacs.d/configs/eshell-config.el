10e2871211b9df394907cd10b6de16d87bb1be27
bastiaba <bastian.ballmann@inf.ethz.ch>
2018-06-25 16:34:48 +0200

    eshell autojump

--- .emacs.d/configs/eshell-config.el
+++ .emacs.d/configs/eshell-config.el
@@ -1,3 +1,9 @@
+(load-file "~/.emacs.d/extensions/eshell-autojump.el")
+(eval-after-load 'eshell
+  '(require 'eshell-autojump nil t))
+
+(setq eshell-last-dir-ring-size 500)
+
 ;; borrowed from https://www.reddit.com/r/emacs/comments/6f0rkz/my_fancy_eshell_prompt/
 ;; and slightly modified. Thanks!
 (setq eshell-prompt-function
@@ -12,7 +18,7 @@
 (propertize (system-name) 'face `(:foreground "white"))
 
 (propertize "]──[" 'face `(:foreground "green"))
-(propertize (concat (eshell/pwd)) 'face `(:foreground "blue"))
+(propertize (concat (eshell/pwd)) 'face `(:foreground "#000099"))
 
 (propertize "]\n" 'face `(:foreground "green"))
 (propertize "└─>" 'face `(:foreground "green"))
