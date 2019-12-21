0a4d075274b7937a1903d11da541db3cdd80232c
Bastian Ballmann <balle@codekid.net>
2019-12-21 21:21:33 +0100

    save personal dict without confirmation

--- .emacs.d/configs/spell-checking-config.el
+++ .emacs.d/configs/spell-checking-config.el
@@ -1,11 +1,20 @@
 ; spell checking
 (require 'ispell)
 
-(setq ispell-dictionary "german")
+(setq ispell-local-dictionary "de_DE")
+(setq ispell-local-dictionary-alist '(("de_DE" "[[:alpha:]]" "[^[:alpha:]]" "[']" nil ("-d" "de_DE") nil utf-8)))
 (setq ispell-silently-savep t)
 (setq-default ispell-program-name "hunspell")
 (setq ispell-really-hunspell t)
 (setq ispell-skip-html t)
+
+(eval-after-load "ispell"
+(progn
+(setq ispell-dictionary "de_DE")
+;(setq ispell-extra-args '("-t")) ; input is tex or latex
+(setq ispell-silently-savep t) ; save personal dict without confirmation
+))
+
 
 (defun turn-on-flyspell-mode ()
    (flyspell-mode 1)
