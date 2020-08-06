ee65904cb5ddef08d65bea3b7f7cf629a538fe00
Bastian Ballmann <balle@codekid.net>
2020-08-06 17:28:48 +0200

    better golang integration

--- .emacs.d/configs/go-config.el
+++ .emacs.d/configs/go-config.el
@@ -1,23 +1,32 @@
-;; $ export GOPATH=~/go/bin
 ;; $ export PATH=$PATH:$GOPATH/bin
-;; $ go get -u golang.org/x/tools/cmd/guru
-;; $ go get github.com/rogpeppe/godef
-;; $ go get -u github.com/nsf/gocode
-;; $ go get -u golang.org/x/tools/cmd/goimports
-;; $ go get -u github.com/dougm/goflymake
+;; $ go get -u -v golang.org/x/tools/cmd/guru
+;; $ go get -u -v github.com/rogpeppe/godef
+;; $ go get -u -v github.com/nsf/gocode
+;; $ go get -u -v golang.org/x/tools/cmd/goimports
+;; $ go get -u -v github.com/dougm/goflymake
+;; $ go get -u -v github.com/motemen/gore
+
+(install-missing-packages '(gotest
+			    gorepl-mode))
 
 
 (add-to-list 'load-path "~/.emacs.d/extensions/go-mode.el")
 (require 'go-mode)
-(require 'go-guru)
 (require 'go-autocomplete)
+(require 'gotest)
+
+;; source code navigator
+(require 'go-guru)
+
+;; a repl for interactive go programming
+(require 'gorepl-mode)
 
 ;; formating options
 (setq tab-width 4)
 (setq standard-indent 4)
 (setq indent-tabs-mode nil)
 
-;; automatically add imports on save
+;; automatically add imports and format source code on save
 (setq gofmt-command "goimports")
 (setq gofmt-args "")
 (add-hook 'before-save-hook 'gofmt-before-save)
