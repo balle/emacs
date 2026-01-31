8f776b17ab406f4eb9a30cea9733d7e5604f0d1e
Bastian Ballmann <balle@codekid.net>
2026-01-31 09:17:14 +0100

    added valgrind command

--- .emacs.d/configs/c-config.el
+++ .emacs.d/configs/c-config.el
@@ -9,12 +9,18 @@
       gdb-show-main t
       lsp-idle-delay 0.1)  ;; clangd is fast
 
+(defun balle-valgrind ()
+  (interactive)
+  (let ((cmd (read-shell-command "Valgrind-Command: " "valgrind build/")))
+    (shell-command cmd))
+)
 
 (add-hook 'c-mode-hook '(lambda ()
 			  (setq c-basic-offset 4
 				tab-width 4
 				indent-tabs-mode t
 				compile-command "cmake -B build && cmake --build build")
-			  (define-key c-mode-map "\C-cc" 'compile)))
+			 (define-key c-mode-map "\C-cc" 'compile)
+			 (define-key c-mode-map "\C-cv" 'balle-valgrind)))
 
 
